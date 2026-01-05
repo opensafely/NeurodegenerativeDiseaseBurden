b16babb94a30de2acca248a8ea79cd9f3c55a321
WoshiSeven <98469272+WoshiSeven@users.noreply.github.com>
2026-01-05 12:30:16 +0000

    Import ethnicity code list to codelists.py

--- analysis/dataset_definition/codelists.py
+++ analysis/dataset_definition/codelists.py
@@ -9,3 +9,10 @@ pregnancy_snomed = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-c19preg_cod.csv",
     column="code"
 )
+
+ethnicity_codelist = codelist_from_csv(
+    "codelists/opensafely-ethnicity-snomed-0removed.csv",
+    column="code",
+    category_column="Label_16",
+)
+

10e6639fda10fe2b059aabd0bf0fe0b8b66767d3
WoshiSeven <98469272+WoshiSeven@users.noreply.github.com>
2026-01-05 12:23:59 +0000

    Import code lists to codelists.py

--- analysis/dataset_definition/codelists.py
+++ analysis/dataset_definition/codelists.py
@@ -1,6 +1,11 @@
 from ehrql import codelist_from_csv
 
-prostate_cancer_snomed = codelist_from_csv(
+prostate_snomed = codelist_from_csv(
     "codelists/user-RochelleKnight-prostate_cancer_snomed.csv",
+    column="code"
+)
+
+pregnancy_snomed = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-c19preg_cod.csv",
     column="code"
 )

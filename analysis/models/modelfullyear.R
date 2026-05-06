ad0e5608b7af076f8f269e8af6852cf5d4cf034e
WoshiSeven <98469272+WoshiSeven@users.noreply.github.com>
2026-05-06 10:51:35 +0000

    extract predictions for different cov level

--- analysis/models/modelfullyear.R
+++ analysis/models/modelfullyear.R
@@ -1,5 +1,5 @@
 library(data.table)
-
+library(emmeans)
 # Define output folder 
 print("Define output folder")
 
@@ -24,14 +24,14 @@ for (i in yrange){
   dataset_name <- paste0(i,"0101_",i,"1231")
   df <- rbind(df, fread(file = paste0(
   "output/calculations/results-byallg-", dataset_name, ".csv"),
-  colClasses = list(factor = c("year", "age", "sex", "region", "imd", "ethnicity", "cms"))
+  colClasses = list(factor = c("age", "sex", "region", "imd", "ethnicity", "cms"))
   )
   )
 }
 
 # Set cov ref level
 df[,c("year", "age", "sex", "region", "imd", "ethnicity", "cms") := .(
-  relevel(year, ref = "2020"),
+  relevel(as.factor(year), ref = as.character(max(year))),
   relevel(age, ref = "40-49"), 
   relevel(sex, ref = "male"), 
   relevel(region, ref = "London"), 
@@ -46,23 +46,54 @@ df[, logdenom := log(denom)]
 # Remove zeros
 df <- df[!(numer==0 | denom==0)]
 
-# Function to fit model and extract coefficients
+# Create table for predictions
+vars <- c("year", "age", "sex", "region", "imd", "ethnicity", "cms")
+pred_dt <- rbindlist(lapply(vars, function(v) {
+  lvls <- df[,levels(get(v))]
+  base <- df[,lapply(.SD, function(x) levels(x)[1]),.SDcols=vars]
+  base[,logdenom := 0]
+  newdata <- base[rep(1, length(lvls)), ]
+  newdata[[v]] = lvls
+  newdata
+}))
+
+# Function to fit model and extract coefficients and predictions
 fitfullmodel <- function(df, outcome, metric){
   data = df[disease == outcome & metric == metric]
   tryCatch({
+      #fit model
       fit <- glm(numer ~ 
-      #year + 
-      age + sex + region + imd + ethnicity + cms, 
+      year + age + sex + region + imd + ethnicity + cms, 
         offset = logdenom, data = data, family = quasipoisson(link = "log")
         )
+      #get model coef
       coefs <- summary(fit)$coefficients
       dt <- as.data.table(coefs, keep.rownames = "term")
       setnames(dt, c("Estimate", "Std. Error", "t value", "Pr(>|t|)"),
                     c("estimate", "std_error", "statistic", "p_value"))
-      dt[, c("disease", "metric", "error") := .(outcome, metric, NA_character_)]
-      setcolorder(dt, c("disease", "metric", "term", "estimate", "std_error", "statistic", "p_value", "error"))
-      dt
-    }, error = function(e) {
+      
+      #get response for different cov comb
+      vars <- c("year", "age", "sex", "region", "imd", "ethnicity", "cms")
+      pred_dt <- rbindlist(lapply(vars, function(v) {
+        lvls <- df[,levels(get(v))]
+        base <- df[,lapply(.SD, function(x) levels(x)[1]),.SDcols=vars]
+        base[,logdenom := 0]
+        newdata <- base[rep(1, length(lvls)), ]
+        newdata[[v]] = lvls
+        rbindlist(lapply(1:nrow(newdata), function(i) {
+          data.table(term=paste0(v,newdata[i,..v]),
+                     pred=predict(fit, newdata = newdata[i, ], type = "response")
+          )                    
+          }))
+      }))
+      #combine results
+      dt2 <- merge(dt, pred_dt, by = "term", all = TRUE)
+      dt2[, c("disease", "metric", "error") := .(outcome, metric, NA_character_)]
+      setcolorder(dt2, c("disease", "metric", "term", "estimate", "std_error", "statistic", "p_value", "pred", "error"))
+      dt2
+    },
+    #get error message if model fails 
+    error = function(e) {
       data.table(
         disease = outcome,
         metric = metric,
@@ -71,6 +102,7 @@ fitfullmodel <- function(df, outcome, metric){
         std_error = NA_real_,
         statistic = NA_real_,
         p_value = NA_real_,
+        pred = NA_real_,
         error = e$message
       )
     }) 

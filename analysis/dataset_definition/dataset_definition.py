f95a5cd5e2d356b2ae43fc6cf5d0746e0b929b3c
WoshiSeven <98469272+WoshiSeven@users.noreply.github.com>
2026-08-21 17:07:22 +0000

    Reassign dementia subtypes

--- analysis/dataset_definition/dataset_definition.py
+++ analysis/dataset_definition/dataset_definition.py
@@ -118,8 +118,8 @@ olists = {
     "dlb": {"snomed": lewybody_snomed},
 }
 olists["dementia"] = {
-    "snomed": specified_dementia_snomed+unspecified_dementia_snomed+alzheimers_snomed+vascular_snomed, 
-    "icd": specified_dementia_icd+unspecified_dementia_icd+alzheimers_icd+vascular_icd
+    "snomed": specified_dementia_snomed+unspecified_dementia_snomed+alzheimers_snomed+vascular_snomed+frontotemporal_snomed+lewybody_snomed, 
+    "icd": specified_dementia_icd+unspecified_dementia_icd+alzheimers_icd+vascular_icd+frontotemporal_icd
 }
 
 # Prevalent status for any dementia before start date
@@ -135,7 +135,7 @@ prevalent_dementia_start = maximum_of(*prev_dementia_start)
 # First incident date among dementia subtypes during follow up by data source
 inci_dementia_source = {}
 inci_dementia_min = {}
-for name in ["osd", "ud", "ad", "vd"]:
+for name in ["osd", "ud", "ad", "vd", "ftd", "dlb"]:
     incident = {}
     if "snomed" in olists[name]:
         incident["primary"] = first_matching_tpp_between(olists[name]["snomed"], start_date, pat_end_date, death_date)
@@ -150,15 +150,80 @@ for name in ["osd", "ud", "ad", "vd"]:
     inci_dementia_source[name] = incident
 inci_dementia_1 = minimum_of(*inci_dementia_min.values())
 inci_dementia_status = case(
-            when(inci_dementia_1 == inci_dementia_min['ad']).then("ad"),
-            when(inci_dementia_1 == inci_dementia_min['vd']).then("vd"),
-            when(inci_dementia_1 == inci_dementia_min['osd']).then("osd"),
-            when(inci_dementia_1 == inci_dementia_min['ud']).then("ud")
+            when(
+                (inci_dementia_1 == inci_dementia_min['ad'])&
+                ((inci_dementia_1 != inci_dementia_min['vd'])|
+                 (inci_dementia_min['vd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['osd'])|
+                 (inci_dementia_min['osd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['ftd'])|
+                 (inci_dementia_min['ftd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['dlb'])|
+                 (inci_dementia_min['dlb'].is_null()))
+                ).then('ad'),
+            when(
+                (inci_dementia_1 == inci_dementia_min['vd'])&
+                ((inci_dementia_1 != inci_dementia_min['ad'])|
+                 (inci_dementia_min['ad'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['osd'])|
+                 (inci_dementia_min['osd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['ftd'])|
+                 (inci_dementia_min['ftd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['dlb'])|
+                 (inci_dementia_min['dlb'].is_null()))
+                ).then('vd'), 
+            when(
+                (inci_dementia_1 == inci_dementia_min['ud'])&
+                ((inci_dementia_1 != inci_dementia_min['vd'])|
+                 (inci_dementia_min['vd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['osd'])|
+                 (inci_dementia_min['osd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['ad'])|
+                 (inci_dementia_min['ud'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['ftd'])|
+                 (inci_dementia_min['ftd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['dlb'])|
+                 (inci_dementia_min['dlb'].is_null()))
+                ).then('ud'),   
+            when(
+                (inci_dementia_1 == inci_dementia_min['ftd'])&
+                ((inci_dementia_1 != inci_dementia_min['vd'])|
+                 (inci_dementia_min['vd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['osd'])|
+                 (inci_dementia_min['osd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['ad'])|
+                 (inci_dementia_min['ftd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['dlb'])|
+                 (inci_dementia_min['dlb'].is_null()))
+                ).then('ftd'), 
+            when(
+                (inci_dementia_1 == inci_dementia_min['dlb'])&
+                ((inci_dementia_1 != inci_dementia_min['vd'])|
+                 (inci_dementia_min['vd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['osd'])|
+                 (inci_dementia_min['osd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['ftd'])|
+                 (inci_dementia_min['ftd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['ad'])|
+                 (inci_dementia_min['dlb'].is_null()))
+                ).then('dlb'), 
+            when(
+                (inci_dementia_1 == inci_dementia_min['ad'])&
+                (inci_dementia_1 == inci_dementia_min['vd'])&
+                ((inci_dementia_1 != inci_dementia_min['osd'])|
+                 (inci_dementia_min['osd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['ftd'])|
+                 (inci_dementia_min['ftd'].is_null()))&
+                ((inci_dementia_1 != inci_dementia_min['dlb'])|
+                 (inci_dementia_min['dlb'].is_null()))
+                ).then('advd_mixed'),
+            when(inci_dementia_1.is_not_null()).then('osd_mixed'),  
+            otherwise = None     
         )
 
-# First incident date among dementia subtypes before mid time point
+# prevalent dementia subtypes before mid time point
 inci_dementia_mid = {}
-for name in ["osd", "ud", "ad", "vd"]:
+for name in ["osd", "ud", "ad", "vd", "ftd", "dlb"]:
     incident = []
     if "snomed" in olists[name]:
         incident.append(
@@ -178,15 +243,80 @@ for name in ["osd", "ud", "ad", "vd"]:
     inci_dementia_mid[name] = tmp_incident_date
 inci_dementia_mid_1 = minimum_of(*inci_dementia_mid.values())
 prevalent_dementia_mid = case(
-            when(inci_dementia_mid_1 == inci_dementia_mid["ad"]).then("ad"),
-            when(inci_dementia_mid_1 == inci_dementia_mid['vd']).then("vd"),
-            when(inci_dementia_mid_1 == inci_dementia_mid['osd']).then("osd"),
-            when(inci_dementia_mid_1 == inci_dementia_mid['ud']).then("ud"),
+            when(
+                (inci_dementia_mid_1 == inci_dementia_mid['ad'])&
+                ((inci_dementia_mid_1 != inci_dementia_mid['vd'])|
+                    (inci_dementia_mid['vd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['osd'])|
+                    (inci_dementia_mid['osd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['ftd'])|
+                    (inci_dementia_mid['ftd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['dlb'])|
+                    (inci_dementia_mid['dlb'].is_null()))
+                ).then('ad'),
+            when(
+                (inci_dementia_mid_1 == inci_dementia_mid['vd'])&
+                ((inci_dementia_mid_1 != inci_dementia_mid['ad'])|
+                    (inci_dementia_mid['ad'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['osd'])|
+                    (inci_dementia_mid['osd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['ftd'])|
+                    (inci_dementia_mid['ftd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['dlb'])|
+                    (inci_dementia_mid['dlb'].is_null()))
+                ).then('vd'), 
+            when(
+                (inci_dementia_mid_1 == inci_dementia_mid['ud'])&
+                ((inci_dementia_mid_1 != inci_dementia_mid['vd'])|
+                    (inci_dementia_mid['vd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['osd'])|
+                    (inci_dementia_mid['osd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['ad'])|
+                    (inci_dementia_mid['ud'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['ftd'])|
+                    (inci_dementia_mid['ftd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['dlb'])|
+                    (inci_dementia_mid['dlb'].is_null()))
+                ).then('ud'),   
+            when(
+                (inci_dementia_mid_1 == inci_dementia_mid['ftd'])&
+                ((inci_dementia_mid_1 != inci_dementia_mid['vd'])|
+                    (inci_dementia_mid['vd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['osd'])|
+                    (inci_dementia_mid['osd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['ad'])|
+                    (inci_dementia_mid['ftd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['dlb'])|
+                    (inci_dementia_mid['dlb'].is_null()))
+                ).then('ftd'), 
+            when(
+                (inci_dementia_mid_1 == inci_dementia_mid['dlb'])&
+                ((inci_dementia_mid_1 != inci_dementia_mid['vd'])|
+                    (inci_dementia_mid['vd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['osd'])|
+                    (inci_dementia_mid['osd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['ftd'])|
+                    (inci_dementia_mid['ftd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['ad'])|
+                    (inci_dementia_mid['dlb'].is_null()))
+                ).then('dlb'), 
+            when(
+                (inci_dementia_mid_1 == inci_dementia_mid['ad'])&
+                (inci_dementia_mid_1 == inci_dementia_mid['vd'])&
+                ((inci_dementia_mid_1 != inci_dementia_mid['osd'])|
+                    (inci_dementia_mid['osd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['ftd'])|
+                    (inci_dementia_mid['ftd'].is_null()))&
+                ((inci_dementia_mid_1 != inci_dementia_mid['dlb'])|
+                    (inci_dementia_mid['dlb'].is_null()))
+                ).then('advd_mixed'), 
+            when(inci_dementia_mid_1.is_not_null()).then('osd_mixed'),  
+            otherwise = None
             )
 
 for name, codes in olists.items():
     # For other neuro disease
-    if name not in ["osd", "ud", "ad", "vd"]:
+    if name not in ["osd", "ud", "ad", "vd", 'ftd', 'dlb']:
         incident = {}
         prevalent_start = []
         prevalent_mid = []
@@ -354,42 +484,43 @@ for name, codes in olists.items():
                 otherwise=0
                 )
             )
-        
-    # For dementia subtypes
-    else:
-        # Prevalence numerator
-        setattr(dataset,
-                f"pnumer_bin_{name}",
-                case(
-                    when((death_date<mid_date)|~(practice_registrations.exists_for_patient_on(mid_date))).then(0),
-                    when(prevalent_dementia_mid == name).then(1),
-                    otherwise=0
-                    )
-        )   
 
-        # Incidence numerator 
-        setattr(
-            dataset,
-            f"inumer_bin_{name}",
-            case(
-                when(prevalent_dementia_start == 1).then(0),
-                when(inci_dementia_status == name).then(1),
-                otherwise=0,
+# For dementia subtypes
+for name in ['ad', 'vd', 'ud', 'ftd', 'dlb', 'advd_mixed', 'osd_mixed']:
+    # Prevalence numerator
+    setattr(dataset,
+        f"pnumer_bin_{name}",
+        case(
+            when((death_date<mid_date)|~(practice_registrations.exists_for_patient_on(mid_date))).then(0),
+            when(prevalent_dementia_mid == name).then(1),
+            otherwise=0
             )
-        )
+    )   
 
-        # Incidence denominator
-        tmp_idenom_num = minimum_of(pat_end_date, inci_dementia_1)
-        setattr(
-            dataset,
-            f"idenom_num_{name}",
-            case(
-                when(prevalent_dementia_start == 1).then(0),
-                otherwise = maximum_of(0,(tmp_idenom_num - start_date).days)
-            )
+    # Incidence numerator 
+    setattr(
+        dataset,
+        f"inumer_bin_{name}",
+        case(
+            when(prevalent_dementia_start == 1).then(0),
+            when(inci_dementia_status == name).then(1),
+            otherwise=0,
         )
+    )
 
-        #Add data source for first incidence
+    # Incidence denominator
+    tmp_idenom_num = minimum_of(pat_end_date, inci_dementia_1)
+    setattr(
+        dataset,
+        f"idenom_num_{name}",
+        case(
+            when(prevalent_dementia_start == 1).then(0),
+            otherwise = maximum_of(0,(tmp_idenom_num - start_date).days)
+        )
+    )
+
+    #Add data source for first incidence
+    if name not in ['advd_mixed', 'osd_mixed']:
         if len(inci_dementia_source[name]) == 1:
             setattr(
                     dataset,
@@ -446,29 +577,30 @@ for name, codes in olists.items():
                         otherwise = 0              
                     )
             )
-        
-        # Case fatality numerator
-        setattr(
-            dataset,
-            f"fnumer_bin_1y_{name}",
-            case(when((getattr(dataset,f"inumer_bin_{name}")==1) & 
-                    ((death_date - inci_dementia_1).years <=1) &
-                    (practice_registrations.exists_for_patient_on(death_date))
-                    ).then(1),
-                otherwise=0
-            )
+    
+    # Case fatality numerator
+    setattr(
+        dataset,
+        f"fnumer_bin_1y_{name}",
+        case(when((getattr(dataset,f"inumer_bin_{name}")==1) & 
+                ((death_date - inci_dementia_1).years <=1) &
+                (practice_registrations.exists_for_patient_on(death_date))
+                ).then(1),
+            otherwise=0
         )
-        
-        setattr(
-            dataset,
-            f"fnumer_bin_5y_{name}",
-            case(when((getattr(dataset,f"inumer_bin_{name}")==1) & 
-                    ((death_date - inci_dementia_1).years <=5) &
-                    (practice_registrations.exists_for_patient_on(death_date))
-                    ).then(1),
-                otherwise=0
-            )
+    )
+    
+    setattr(
+        dataset,
+        f"fnumer_bin_5y_{name}",
+        case(when((getattr(dataset,f"inumer_bin_{name}")==1) & 
+                ((death_date - inci_dementia_1).years <=5) &
+                (practice_registrations.exists_for_patient_on(death_date))
+                ).then(1),
+            otherwise=0
         )
+    )
+
 # Prevalence denominator - population at mid date
 dataset.pdenom_bin_mid = case(when(((death_date>=mid_date) | (death_date.is_null())) &
                                    (practice_registrations.exists_for_patient_on(mid_date))
